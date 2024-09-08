ceed6fb9b2252ac9a7e7abbb4c8748c9ecf810d8
amany <amany@gmail.com>
2024-09-08 18:11:32 +0300

    note item finish

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
+import 'package:notes_app/widgets/note_item.dart';
 
 class notesViewBody extends StatelessWidget {
   const notesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-         customAppBar(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          customAppBar(),
+          noteItem(),
+        ],
+      ),
     );
   }
 }
-
-
