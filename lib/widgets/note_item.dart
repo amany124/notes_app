5ee20fa036785bc9178ee88836743dc2b9abbcfc
amany <amany@gmail.com>
2024-09-22 20:06:58 +0300

    delete note and refresh the list

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/cubits/display_notes_cubit/display_notes_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
@@ -42,7 +44,11 @@ class noteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                  BlocProvider.of<displayNotesCubit>(context)
+                      .displayNotesMethod();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
